c49106034371f686ef7b1bcbc92b0bc2d3b4b4ce
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-06-12 20:30:45 -0700

    Basic container on fargate

--- index.ts
+++ index.ts
@@ -2,8 +2,38 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const repo = new awsx.ecr.Repository("repo", {
+    forceDelete: true,
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const image = new awsx.ecr.Image("image", {
+    repositoryUrl: repo.url,
+    context: "./app",
+    platform: "linux/amd64",
+});
+
+const cluster = new aws.ecs.Cluster("cluster");
+
+const lb = new awsx.lb.ApplicationLoadBalancer("lb");
+
+const service = new awsx.ecs.FargateService("service", {
+    cluster: cluster.arn,
+    assignPublicIp: true,
+    taskDefinitionArgs: {
+        container: {
+            name: "my-service",
+            image: image.imageUri,
+            cpu: 128,
+            memory: 512,
+            essential: true,
+            portMappings: [
+                {
+                    containerPort: 80,
+                    targetGroup: lb.defaultTargetGroup,
+                },
+            ],
+        },
+    },
+});
+
+export const url = pulumi.interpolate`http://${lb.loadBalancer.dnsName}`;
